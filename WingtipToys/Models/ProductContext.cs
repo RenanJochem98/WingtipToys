14c7fb0a5eb2fc05863c6051cd64e685acd73a39
Renan Jochem <renanjochem98@gmail.com>
2020-01-23 10:02:39 -0300

    Adicionando carrinho no acesso ao banco de dados

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -6,6 +6,7 @@ using System.Data.Entity; // necessario instalar via gerenciador de pacotes
 
 namespace WingtipToys.Models
 {
+    // Unica classe responsavel pelo acesso ao banco de dados diretamente. acesso feito com Entity Framework
     public class ProductContext : DbContext
     {
         public ProductContext() : base("WingtipToys")
@@ -13,5 +14,6 @@ namespace WingtipToys.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
     }
 }
